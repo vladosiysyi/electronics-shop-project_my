4b64f2efa1aa8e0f3559ba839ea72f7f8c3e8c69
vlad <chiklow@mail.ru>
2023-08-20 21:19:16 +0300

    init

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import math
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,35 @@ class Item:
         :param quantity: Количество товара в магазине.
         """
 
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         # pass
+    @property
+    def pr_name(self):
+        return self.__name
+
+    @pr_name.setter
+    def pr_name(self, short_name):
+        if len(self.__name) > 10:
+            print('Длина наименования товара превышает 10 символов')
+            short_name = self.__name[:10]
+        return short_name
+
+    @staticmethod
+    def string_to_number(num):
+        i_num = float(num)
+        return math.floor(i_num)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                print(row['name'], row['price'], row['quantity'])
+                cls(row['name'], row['price'], row['quantity'])
+
 
     def calculate_total_price(self) -> float:
         """
@@ -34,6 +61,7 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = float(self.price * self.pay_rate)
+
 
 it1 = Item("Смартфон", 10000, 20)
 it2 = Item("Ноутбук", 20000, 5)
